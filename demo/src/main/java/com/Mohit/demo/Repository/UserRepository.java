2dbd8aed204c658cc26f4a0c252bbf4583da97f0
Mohitoli <153801521+Mohitoli@users.noreply.github.com>
2024-04-21 23:00:29 +0530

    Delete demo directory

--- demo/src/main/java/com/Mohit/demo/Repository/UserRepository.java
+++
@@ -1,8 +0,0 @@
-package com.Mohit.demo.Repository;
-
-import com.Mohit.demo.Entities.UserEntity;
-import org.apache.catalina.User;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserEntity,Integer> {
-}
