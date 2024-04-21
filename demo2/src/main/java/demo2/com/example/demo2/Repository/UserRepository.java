0df6321ca17209532570af0286972e7c89290cd7
Mohitoli <153801521+Mohitoli@users.noreply.github.com>
2024-04-21 23:01:05 +0530

    Delete demo2 directory

--- demo2/src/main/java/demo2/com/example/demo2/Repository/UserRepository.java
+++
@@ -1,7 +0,0 @@
-package demo2.com.example.demo2.Repository;
-
-import demo2.com.example.demo2.Entity.UserEntity;
-import org.springframework.data.repository.CrudRepository;
-
-public interface UserRepository extends CrudRepository<UserEntity,Integer> {
-}
